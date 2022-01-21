352e25c01f95c5ada87ffc92314ed21b00ec1784
liviaalvzs <livs.santana01@gmail.com>
2022-01-21 01:25:31 -0300

    ajustando a remoção p/ novos pacientes

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -1,7 +1,9 @@
 var pacientes = document.querySelectorAll(".paciente");
+var tabela = document.querySelector("table");
+tabela.addEventListener("dblclick", function(event){
+    var alvoEvento = event.target;
+    var paiDoAlvo = alvoEvento.parentNode;
+    paiDoAlvo.remove();
 
-pacientes.forEach(function(paciente){
-    paciente.addEventListener("dblclick", function(){
-        this.remove();
-    });
+
 });
